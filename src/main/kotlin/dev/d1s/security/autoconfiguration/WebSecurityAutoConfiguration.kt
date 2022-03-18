ba3218dd6009b47413d386748d3468049653df80
d1snin <me@d1s.dev>
2022-03-19 02:36:15 +0300

    tried to exclude spring security autoconfiguration

--- src/main/kotlin/dev/d1s/security/autoconfiguration/WebSecurityAutoConfiguration.kt
+++ src/main/kotlin/dev/d1s/security/autoconfiguration/WebSecurityAutoConfiguration.kt
@@ -1,14 +1,9 @@
 package dev.d1s.security.autoconfiguration
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
-public class WebSecurityAutoConfiguration : WebSecurityConfigurerAdapter() {
-
-    override fun configure(http: HttpSecurity) {
-        // not using spring security for requests authorization
-        http.authorizeRequests().anyRequest().permitAll()
-    }
-}
+@EnableAutoConfiguration(exclude = [SecurityAutoConfiguration::class])
+public class WebSecurityAutoConfiguration
