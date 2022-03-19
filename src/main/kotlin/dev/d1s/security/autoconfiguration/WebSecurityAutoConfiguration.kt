ac13f6e60e1fc64348ed54a7e29da3c28209bb9e
d1snin <me@d1s.dev>
2022-03-19 03:01:18 +0300

    returned to security autoconfig exclusion

--- src/main/kotlin/dev/d1s/security/autoconfiguration/WebSecurityAutoConfiguration.kt
+++ src/main/kotlin/dev/d1s/security/autoconfiguration/WebSecurityAutoConfiguration.kt
@@ -1,19 +1,9 @@
 package dev.d1s.security.autoconfiguration
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
-public class WebSecurityAutoConfiguration : WebSecurityConfigurerAdapter() {
-
-    override fun configure(http: HttpSecurity) {
-        http.oauth2Login().disable()
-            .httpBasic().disable()
-            .cors().disable()
-            .csrf().disable()
-            .sessionManagement().disable()
-            .formLogin().disable()
-            .logout().disable()
-    }
-}
+@EnableAutoConfiguration(exclude = [SecurityAutoConfiguration::class])
+public class WebSecurityAutoConfiguration
