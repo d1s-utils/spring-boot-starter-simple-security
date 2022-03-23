d22434fd7ad8ccc7c58d9876371d9ac9808c316e
d1snin <me@d1s.dev>
2022-03-23 16:45:51 +0300

    imported autoconfiguration for properties

--- src/main/kotlin/dev/d1s/security/configuration/annotation/EnableSimpleSecurity.kt
+++ src/main/kotlin/dev/d1s/security/configuration/annotation/EnableSimpleSecurity.kt
@@ -1,9 +1,13 @@
 package dev.d1s.security.configuration.annotation
 
+import dev.d1s.security.autoconfiguration.ConfigurationPropertiesAutoConfiguration
 import dev.d1s.security.autoconfiguration.SimpleSecurityAutoConfiguration
 import org.springframework.context.annotation.Import
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@Import(SimpleSecurityAutoConfiguration::class)
+@Import(
+    SimpleSecurityAutoConfiguration::class,
+    ConfigurationPropertiesAutoConfiguration::class
+)
 public annotation class EnableSimpleSecurity
