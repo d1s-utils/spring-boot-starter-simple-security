63770398dd89b12f808f5e081df46bf3affa2a91
d1snin <me@d1s.dev>
2022-03-23 18:41:19 +0300

    moved aspect bean declaration to `AopAutoConfiguration`

--- src/main/kotlin/dev/d1s/security/autoconfiguration/AopAutoConfiguration.kt
+++ src/main/kotlin/dev/d1s/security/autoconfiguration/AopAutoConfiguration.kt
@@ -1,8 +1,14 @@
 package dev.d1s.security.autoconfiguration
 
+import dev.d1s.security.aop.SecuredControllerMethodAspect
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @Configuration
 @EnableAspectJAutoProxy
-public class AopAutoConfiguration
+public class AopAutoConfiguration {
+
+    @Bean
+    internal fun securedControllerMethodAspect() = SecuredControllerMethodAspect()
+}
--- src/main/kotlin/dev/d1s/security/autoconfiguration/SimpleSecurityAutoConfiguration.kt
+++ src/main/kotlin/dev/d1s/security/autoconfiguration/SimpleSecurityAutoConfiguration.kt
@@ -1,18 +1,14 @@
 package dev.d1s.security.autoconfiguration
 
+import dev.d1s.security.service.impl.SimpleAuthorizationServiceImpl
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import dev.d1s.security.aop.SecuredControllerMethodAspect
-import dev.d1s.security.service.impl.SimpleAuthorizationServiceImpl
 
 @Configuration
 @ConditionalOnProperty("simple-security.enabled")
 public class SimpleSecurityAutoConfiguration {
-
-    @Bean
-    internal fun securedControllerMethodAspect() = SecuredControllerMethodAspect()
 
     @Bean
     @ConditionalOnMissingBean
