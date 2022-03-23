496db9536d9c9a99e6dd83a012758f964bc10d3c
d1snin <me@d1s.dev>
2022-03-23 14:18:24 +0300

    autoconfiguring AOP

---
+++ src/main/kotlin/dev/d1s/security/autoconfiguration/AopAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package dev.d1s.security.autoconfiguration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AopAutoConfiguration
