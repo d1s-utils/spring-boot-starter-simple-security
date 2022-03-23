d17a2d6fc2a1469be89edb58bee9e44d40950204
d1snin <me@d1s.dev>
2022-03-23 17:45:09 +0300

    reworked configuration properties (again)

--- src/main/kotlin/dev/d1s/security/properties/SimpleSecurityConfigurationProperties.kt
+++ src/main/kotlin/dev/d1s/security/properties/SimpleSecurityConfigurationProperties.kt
@@ -1,12 +1,16 @@
 package dev.d1s.security.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.validation.annotation.Validated
 import javax.validation.constraints.NotBlank
 
 @Validated
+@ConstructorBinding
 @ConfigurationProperties("simple-security")
-internal class SimpleSecurityConfigurationProperties(
-    var enabled: Boolean = true,
-    @NotBlank var authenticationSecret: String? = null
+internal data class SimpleSecurityConfigurationProperties(
+    val enabled: Boolean = true,
+
+    @NotBlank
+    val authenticationSecret: String
 )
