8a82152d495c24088b06f7d1315f2756c2c04ad3
d1snin <me@d1s.dev>
2022-03-23 15:03:01 +0300

    removed `WebSecurityAutoConfiguration` from the import

--- src/main/kotlin/dev/d1s/security/configuration/annotation/EnableSimpleSecurity.kt
+++ src/main/kotlin/dev/d1s/security/configuration/annotation/EnableSimpleSecurity.kt
@@ -2,14 +2,12 @@ package dev.d1s.security.configuration.annotation
 
 import dev.d1s.security.autoconfiguration.ConfigurationPropertiesAutoConfiguration
 import dev.d1s.security.autoconfiguration.SimpleSecurityAutoConfiguration
-import dev.d1s.security.autoconfiguration.WebSecurityAutoConfiguration
 import org.springframework.context.annotation.Import
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @Import(
     SimpleSecurityAutoConfiguration::class,
-    ConfigurationPropertiesAutoConfiguration::class,
-    WebSecurityAutoConfiguration::class
+    ConfigurationPropertiesAutoConfiguration::class
 )
 public annotation class EnableSimpleSecurity
