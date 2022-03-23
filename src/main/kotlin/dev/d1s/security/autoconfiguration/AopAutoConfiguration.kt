353252d0b5bf8cd465233d8a9befcdb76060838a
d1snin <me@d1s.dev>
2022-03-23 19:32:19 +0300

    made `AopConfigurationProperties` conditional on property `simple-security.enabled`

--- src/main/kotlin/dev/d1s/security/autoconfiguration/AopAutoConfiguration.kt
+++ src/main/kotlin/dev/d1s/security/autoconfiguration/AopAutoConfiguration.kt
@@ -1,12 +1,14 @@
 package dev.d1s.security.autoconfiguration
 
 import dev.d1s.security.aop.SecuredControllerMethodAspect
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @Configuration
 @EnableAspectJAutoProxy
+@ConditionalOnProperty("simple-security.enabled")
 public class AopAutoConfiguration {
 
     @Bean
